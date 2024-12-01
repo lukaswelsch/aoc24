b39d5fd90b682fed7f8057d7957e302084bf246e
Lukas Welsch <lkswlsch@gmail.com>
2024-12-01 12:42:17 +0100

    add some comments

--- 01/part1.sql
+++ 01/part1.sql
@@ -1,3 +1,4 @@
+-- duckdb can't use a delimiter of more than 1 byte, so this will use a regex to load the data
 WITH data AS (
     SELECT string_split_regex(column0, '   ') as arr
         FROM read_csv_auto('input.csv', header=false, delim='')
@@ -6,6 +7,8 @@ extracted AS (
     SELECT arr[1]::INTEGER as first, arr[2]::INTEGER as second
     FROM data
 ),
+-- the puzzle needs us to independently sort the columns
+-- for this we have to create a sort column for each column
 extracted_first AS (
     SELECT first, ROW_NUMBER() OVER (ORDER BY first ASC) as rn
     FROM extracted
