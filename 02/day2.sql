40af5a874848bc188e67634c387212f76311a6a5
Lukas Welsch <lkswlsch@gmail.com>
2024-12-02 22:11:18 +0100

    last optimization

--- 02/day2.sql
+++ 02/day2.sql
@@ -1,59 +1,48 @@
 WITH data AS (
     SELECT
         string_split_regex(column0, ' ') AS arr,
-        row_number() OVER (ORDER BY 1) AS id
+        row_number() OVER () AS id
     FROM read_csv_auto('input.csv', header = false, delim = '')
 ),
 
-temp AS (
-    WITH expanded AS (
-        SELECT
-            id,
-            arr,
-            unnest(arr),
-            generate_subscripts(arr, 1) AS index,
-            generate_series(1, len(arr)) AS idx
-        FROM
-            data
-    )
+expanded AS (
+    SELECT
+        id,
+        arr,
+        unnest(arr),
+        generate_subscripts(arr, 1) AS index,
+        generate_series(1, len(arr)) AS idx
+    FROM
+        data
+),
 
+temp AS (
     SELECT
         id,
         'part1' AS info,
         arr,
-        row_number() OVER (ORDER BY 1) AS slice_id
+        row_number() OVER () * -1 AS slice_id
     FROM expanded
     UNION ALL
     SELECT
         id,
-        info,
-        arr,
-        slice_id * -1
-    FROM (
-        SELECT
-            id,
-            'part2' AS info,
-            list_select(arr, list_filter(idx, x -> x != index)) AS arr,
-            row_number() OVER (ORDER BY 1) AS slice_id
-        FROM
-            expanded
-    )
+        'part2' AS info,
+        list_select(arr, list_filter(idx, x -> x != index)) AS arr,
+        row_number() OVER () AS slice_id
+    FROM
+        expanded
 ),
 
 paired_values AS (
     SELECT
-        id,
-        slice_id,
-        info,
+        *,
         unnest(array_zip(arr, arr[2:-1])[1:-2]) AS pair
     FROM temp
 ),
 
 validation AS (
     SELECT
-        id,
-        slice_id,
-        info,
+        *,
         CASE
             WHEN abs(pair[1]::INTEGER - pair[2]::INTEGER) IN (1, 2, 3) -- Differences must be 1 or 2 or 3
                 THEN 1
@@ -74,9 +63,7 @@ group_validation AS (
 
 ordered_rows AS (
     SELECT
-        id,
-        slice_id,
-        info,
+        *,
         unnest(arr) AS number,
         generate_subscripts(arr, 1) AS index
     FROM temp
@@ -118,7 +105,7 @@ grouped AS (
 
 SELECT
     info,
-    count(*)
+    count(*) AS result
 FROM grouped
 GROUP BY info
 ORDER BY info
