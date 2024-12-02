e3e22014a3eb91e583222c8b267367be5d4b4aad
Lukas Welsch <lkswlsch@gmail.com>
2024-12-02 21:48:15 +0100

    include part 1 and 2 in one sql

--- 02/day2.sql
+++ 02/day2.sql
@@ -1,4 +1,3 @@
-CREATE OR REPLACE TABLE t1 AS
 WITH data AS (
     SELECT
         string_split_regex(column0, ' ') AS arr,
@@ -6,67 +5,120 @@ WITH data AS (
     FROM read_csv_auto('input.csv', header = false, delim = '')
 ),
 
-check_validity AS (
+temp AS (
+    WITH expanded AS (
+        SELECT
+            id,
+            arr,
+            unnest(arr),
+            generate_subscripts(arr, 1) AS index,
+            generate_series(1, len(arr)) AS idx
+        FROM
+            data
+    )
+
     SELECT
         id,
-        array_zip(arr, arr[2:-1])[1:-2] AS paired_values
-    FROM data
+        'part1' AS info,
+        arr,
+        row_number() OVER (ORDER BY 1) AS slice_id
+    FROM expanded
+    UNION ALL
+    SELECT
+        id,
+        info,
+        arr,
+        slice_id * -1
+    FROM (
+        SELECT
+            id,
+            'part2' AS info,
+            list_select(arr, list_filter(idx, x -> x != index)) AS arr,
+            row_number() OVER (ORDER BY 1) AS slice_id
+        FROM
+            expanded
+    )
 ),
 
 paired_values AS (
     SELECT
-        *,
-        unnest(paired_values) AS pair
-    FROM check_validity
+        id,
+        slice_id,
+        info,
+        unnest(array_zip(arr, arr[2:-1])[1:-2]) AS pair
+    FROM temp
 ),
 
 validation AS (
     SELECT
+        id,
+        slice_id,
+        info,
         CASE
             WHEN abs(pair[1]::INTEGER - pair[2]::INTEGER) IN (1, 2, 3) -- Differences must be 1 or 2 or 3
                 THEN 1
             ELSE 0
-        END AS is_valid_row,
-        id
+        END AS is_valid_row
     FROM paired_values
+),
+
+group_validation AS (
+    SELECT
+        slice_id,
+        id,
+        info
+    FROM validation
+    GROUP BY id, slice_id, info
+    HAVING min(is_valid_row) > 0
 ),
 
 ordered_rows AS (
     SELECT
         id,
+        slice_id,
+        info,
         unnest(arr) AS number,
         generate_subscripts(arr, 1) AS index
+    FROM temp
+),
 
-    FROM data
+prepared_data AS (
+    SELECT
+        *,
+        CASE
+            WHEN number::INTEGER > lag(number::INTEGER) OVER (PARTITION BY id ORDER BY slice_id, index) THEN 'increasing'
+            WHEN number::INTEGER < lag(number::INTEGER) OVER (PARTITION BY id ORDER BY slice_id, index) THEN 'decreasing'
+            ELSE 'same'
+        END AS trend
+    FROM ordered_rows
 ),
 
 ordered_data AS (
-    SELECT id FROM (
-        SELECT
-            *,
-
-            CASE
-                WHEN number::INTEGER > lag(number::INTEGER) OVER (PARTITION BY id ORDER BY index) THEN 'increasing'
-                WHEN number::INTEGER < lag(number::INTEGER) OVER (PARTITION BY id ORDER BY index) THEN 'decreasing'
-                ELSE 'same'
-            END AS trend
-        FROM ordered_rows
-
-    )
+    SELECT
+        id,
+        slice_id,
+        info
+    FROM prepared_data
     WHERE index != 1
-    GROUP BY id
+    GROUP BY id, slice_id, info
     HAVING count(DISTINCT trend) = 1
-
 ),
 
 grouped AS (
-    SELECT o.id
-    FROM validation AS v
-    INNER JOIN ordered_data AS o ON v.id = o.id
-    GROUP BY o.id
-    HAVING min(is_valid_row) > 0
+    SELECT
+        o.id,
+        o.info
+    FROM group_validation AS gv
+    INNER JOIN ordered_data AS o
+        ON
+            gv.id = o.id
+            AND gv.slice_id = o.slice_id
+    GROUP BY o.id, o.info
 )
--- SELECT COUNT(*) as result, 'part1' AS info
--- FROM grouped
 
-SELECT * FROM grouped
+SELECT
+    info,
+    count(*)
+FROM grouped
+GROUP BY info
+ORDER BY info
--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 cd 02
 
-#duckdb < day1.sql
-duckdb < day2.sql test.db
-duckdb < day2_2.sql test.db
+duckdb < day1.sql
+duckdb < day2.sql
